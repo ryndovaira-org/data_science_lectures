179e8ff5c265b2e0127ba099f474d49da7c0da06
ryndovaira <ryndovaira@gmail.com>
2021-01-30 22:23:46 +0300

    Update

--- 2_data_engineering/lectures/scrapy_lecture_project/scrapy_lecture_project/spiders/habr.py
+++ 2_data_engineering/lectures/scrapy_lecture_project/scrapy_lecture_project/spiders/habr.py
@@ -1,23 +1,41 @@
 import scrapy
-from scrapy import Selector, Request
+from scrapy import Request
+
+
+class HabrPost(scrapy.Item):
+    header = scrapy.Field()
+    link = scrapy.Field()
+
+
+class HabrNews(scrapy.Item):
+    header = scrapy.Field()
+    link = scrapy.Field()
+
+
+class HabrHub(scrapy.Item):
+    header = scrapy.Field()
+    link = scrapy.Field()
+    subscribers_count = scrapy.Field()
+    rating_cout = scrapy.Field()
+
+class HabrAuthor(scrapy.Item):
+    fullname = scrapy.Field()
+    nickname = scrapy.Field()
+    link = scrapy.Field()
+    karma_count = scrapy.Field()
+    rating_cout = scrapy.Field()
+
+class HabrCompany(scrapy.Item):
+    header = scrapy.Field()
+    link = scrapy.Field()
+    subscribers_count = scrapy.Field()
+    rating_cout = scrapy.Field()
 
 
 class HabrSpider(scrapy.Spider):
     name = 'habr'
     allowed_domains = ['habr.com']
     start_urls = ['https://habr.com/ru/top/']
-
-    companies = []
-
-    # [{'text': 'blablabla', 'href': 'https://....'}]
-    posts = []
-
-    # [{'text': 'blablabla', 'href': 'https://....'}]
-    news = []
-
-    hubs = []
-
-    authors = []
 
     def parse(self, response):
         for navigation_link in response.css('a.tabs-menu__item_link'):
@@ -36,9 +54,10 @@ class HabrSpider(scrapy.Spider):
 
     def parse_item_top(self, response):
         for post in response.css('a.post__title_link'):
-            text = post.css('a::text')[0].root.strip()
-            href = post.css('a::attr(href)')[0].root.strip()
-            self.posts.append({'text': text, 'href': href})
+            item = HabrPost()
+            item['header'] = post.css('a::text')[0].root.strip()
+            item['link'] = post.css('a::attr(href)')[0].root.strip()
+            yield item
 
         next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
         if len(next_page) > 0:
@@ -46,9 +65,10 @@ class HabrSpider(scrapy.Spider):
 
     def parse_item_news(self, response):
         for news in response.css('a.post__title_link'):
-            text = news.css('a::text')[0].root.strip()
-            href = news.css('a::attr(href)')[0].root.strip()
-            self.news.append({'text': text, 'href': href})
+            item = HabrNews()
+            item['header'] = news.css('a::text')[0].root.strip()
+            item['link'] = news.css('a::attr(href)')[0].root.strip()
+            yield item
 
         next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
         if len(next_page) > 0:
@@ -56,15 +76,12 @@ class HabrSpider(scrapy.Spider):
 
     def parse_item_hubs(self, response):
         for hub in response.css('li.content-list__item_hubs'):
-            text = hub.css('a.list-snippet__title-link::text')[0].root.strip()
-            href = hub.css('a.list-snippet__title-link::attr(href)')[0].root.strip()
-            subscribers_count = hub.css('div.stats__counter_subscribers::text')[0].root.strip()
-            rating_cout = hub.css('div.stats__counter_rating::text')[0].root.strip()
-
-            self.hubs.append({'text': text,
-                              'href': href,
-                              'subscribers_count': subscribers_count,
-                              'rating_cout': rating_cout})
+            item = HabrHub()
+            item['header'] = hub.css('a.list-snippet__title-link::text')[0].root.strip()
+            item['link'] = hub.css('a.list-snippet__title-link::attr(href)')[0].root.strip()
+            item['subscribers_count'] = hub.css('div.stats__counter_subscribers::text')[0].root.strip()
+            item['rating_cout'] = hub.css('div.stats__counter_rating::text')[0].root.strip()
+            yield item
 
         next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
         if len(next_page) > 0:
@@ -72,17 +89,13 @@ class HabrSpider(scrapy.Spider):
 
     def parse_item_authors(self, response):
         for author in response.css('li.content-list__item_users'):
-            text = author.css('a.list-snippet__fullname::text')[0].root.strip()
-            nick = author.css('a.list-snippet__nickname::attr(href)')[0].root.strip()
-            href = author.css('a.list-snippet__fullname::attr(href)')[0].root.strip()
-            karma_count = author.css('div.stats__counter_karma::text')[0].root.strip()
-            rating_cout = author.css('div.stats__counter_rating::text')[0].root.strip()
-
-            self.authors.append({'text': text,
-                                 'nick': nick,
-                                 'href': href,
-                                 'karma_count': karma_count,
-                                 'rating_cout': rating_cout})
+            item = HabrAuthor()
+            item['fullname'] = author.css('a.list-snippet__fullname::text')[0].root.strip()
+            item['nickname'] = author.css('a.list-snippet__nickname::attr(href)')[0].root.strip()
+            item['link'] = author.css('a.list-snippet__fullname::attr(href)')[0].root.strip()
+            item['karma_count'] = author.css('div.stats__counter_karma::text')[0].root.strip()
+            item['rating_cout'] = author.css('div.stats__counter_rating::text')[0].root.strip()
+            yield item
 
         next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
         if len(next_page) > 0:
@@ -90,15 +103,12 @@ class HabrSpider(scrapy.Spider):
 
     def parse_item_companies(self, response):
         for company in response.css('li.content-list__item_companies'):
-            text = company.css('a.list-snippet__title-link::text')[0].root.strip()
-            href = company.css('a.list-snippet__title-link::attr(href)')[0].root.strip()
-            subscribers_count = company.css('div.stats__counter_subscribers::text')[0].root.strip()
-            rating_cout = company.css('div.stats__counter_rating::text')[0].root.strip()
-
-            self.companies.append({'text': text,
-                                   'href': href,
-                                   'subscribers_count': subscribers_count,
-                                   'rating_cout': rating_cout})
+            item = HabrCompany()
+            item['header'] = company.css('a.list-snippet__title-link::text')[0].root.strip()
+            item['link'] = company.css('a.list-snippet__title-link::attr(href)')[0].root.strip()
+            item['subscribers_count'] = company.css('div.stats__counter_subscribers::text')[0].root.strip()
+            item['rating_cout'] = company.css('div.stats__counter_rating::text')[0].root.strip()
+            yield item
 
         next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
         if len(next_page) > 0:
