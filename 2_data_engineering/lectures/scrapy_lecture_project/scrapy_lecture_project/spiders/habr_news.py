f4960af0614d6420b507d91a59bf726d15b4c2fe
ryndovaira <ryndovaira@gmail.com>
2021-02-01 14:28:00 +0300

    Update

--- 2_data_engineering/lectures/scrapy_lecture_project/scrapy_lecture_project/spiders/habr_news.py
+++ 2_data_engineering/lectures/scrapy_lecture_project/scrapy_lecture_project/spiders/habr_news.py
@@ -8,86 +8,46 @@ class HabrNews(scrapy.Item):
 
 
 class HabrNewsSpider(scrapy.Spider):
-    name = 'habr'
+    name = 'habr_news'
     allowed_domains = ['habr.com']
     start_urls = ['https://habr.com/ru/news/']
 
     def parse(self, response):
-        for navigation_link in response.css('a.tabs-menu__item_link'):
-            text = navigation_link.css('h3.tabs-menu__item-text::text')[0].root.strip()
-            href = navigation_link.css('a::attr(href)')[0].root.strip()
-            if text == 'Статьи':
-                yield Request(href, callback=self.parse_item_top)
-            elif text == 'Новости':
-                yield Request(href, callback=self.parse_item_news)
-            elif text == 'Хабы':
-                yield Request(href, callback=self.parse_item_hubs)
-            elif text == 'Авторы':
-                yield Request(href, callback=self.parse_item_authors)
-            elif text == 'Компании':
-                yield Request(href, callback=self.parse_item_companies)
-
-    def parse_item_top(self, response):
-        for post in response.css('a.post__title_link'):
-            item = HabrPost()
-            item['header'] = post.css('a::text')[0].root.strip()
-            item['link'] = post.css('a::attr(href)')[0].root.strip()
-            yield item
-
-        next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
-        if len(next_page) > 0:
-            yield Request('https://habr.com' + next_page[0].root.strip(), callback=self.parse_item_top)
-
-    def parse_item_news(self, response):
         for news in response.css('a.post__title_link'):
-            item = HabrNews()
-            item['header'] = news.css('a::text')[0].root.strip()
-            item['link'] = news.css('a::attr(href)')[0].root.strip()
-            yield item
+            # item = HabrNews()
+            # item['header'] = news.css('a::text')[0].root.strip()
+            # item['link'] = news.css('a::attr(href)')[0].root.strip()
+            # yield item
+            link = news.css('a::attr(href)')[0].root.strip()
+            yield Request(link, callback=self.parse_item_news)
 
         next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
         if len(next_page) > 0:
             yield Request('https://habr.com' + next_page[0].root.strip(), callback=self.parse_item_news)
 
-    def parse_item_hubs(self, response):
-        for hub in response.css('li.content-list__item_hubs'):
-            item = HabrHub()
-            item['header'] = hub.css('a.list-snippet__title-link::text')[0].root.strip()
-            item['link'] = hub.css('a.list-snippet__title-link::attr(href)')[0].root.strip()
-            item['subscribers_count'] = hub.css('div.stats__counter_subscribers::text')[0].root.strip()
-            item['rating_cout'] = hub.css('div.stats__counter_rating::text')[0].root.strip()
-            yield item
+    def parse_item_news(self, response):
+        # TODO: Проверки
+        news_id = response.url.split('/')[-2]
+        title = response.css('span.post__title-text::text')[0].root.strip()
+        hubs = []
+        text = []
+        tags = []
+        for hub in response.css('a.hub-link::text'):
+            hubs.append(hub.root.strip())
 
-        next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
-        if len(next_page) > 0:
-            yield Request('https://habr.com' + next_page[0].root.strip(), callback=self.parse_item_hubs)
+        for text_part in response.css('div.post__text').css('p::text'):
+            text.append(text_part.root.strip())
 
-    def parse_item_authors(self, response):
-        for author in response.css('li.content-list__item_users'):
-            item = HabrAuthor()
-            item['fullname'] = author.css('a.list-snippet__fullname::text')[0].root.strip()
-            item['nickname'] = author.css('a.list-snippet__nickname::attr(href)')[0].root.strip()
-            item['link'] = author.css('a.list-snippet__fullname::attr(href)')[0].root.strip()
-            item['karma_count'] = author.css('div.stats__counter_karma::text')[0].root.strip()
-            item['rating_cout'] = author.css('div.stats__counter_rating::text')[0].root.strip()
-            yield item
+        for tag in response.css('a.post__tag::text'):
+            tags.append(tag.root.strip())
 
-        next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
-        if len(next_page) > 0:
-            yield Request('https://habr.com' + next_page[0].root.strip(), callback=self.parse_item_authors)
+        author = response.css('span.user-info__nickname::text')[0].root.strip()
+        author_specialization = response.css('div.user-info__specialization::text')[0].root.strip()
+        author_karma = response.css('div.stacked-counter__value_green::text')[0].root.strip().replace(',', '.')
+        author_rating = response.css('div.stacked-counter__value_magenta::text')[0].root.strip().replace(',', '.')
 
-    def parse_item_companies(self, response):
-        for company in response.css('li.content-list__item_companies'):
-            item = HabrCompany()
-            item['header'] = company.css('a.list-snippet__title-link::text')[0].root.strip()
-            item['link'] = company.css('a.list-snippet__title-link::attr(href)')[0].root.strip()
-            item['subscribers_count'] = company.css('div.stats__counter_subscribers::text')[0].root.strip()
-            item['rating_cout'] = company.css('div.stats__counter_rating::text')[0].root.strip()
-            yield item
-
-        next_page = response.css('a.arrows-pagination__item-link_next::attr(href)')
-        if len(next_page) > 0:
-            yield Request('https://habr.com' + next_page[0].root.strip(), callback=self.parse_item_companies)
+        comments_counter = response.css('span.comments-section__head-counter::text')[0].root.strip()
+        ...
 
 
 if __name__ == '__main__':
