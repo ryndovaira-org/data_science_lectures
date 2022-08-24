f19df70d054d644174b7be13e9abfaf3e4b027a7
ryndovaira <ryndovaira@gmail.com>
2022-08-24 13:39:39 +0400

    update

--- create_toc.py
+++ create_toc.py
@@ -2,32 +2,58 @@ import json
 import os
 from glob import glob
 
-filepaths = [y for x in os.walk('.') for y in glob(os.path.join(x[0], '*.ipynb'))]
 
-filepaths.sort()
-print(filepaths)
+def get_filepaths():
+    file_paths = [y for x in os.walk('.') for y in glob(os.path.join(x[0], '*.ipynb'))]
+    file_paths.sort()
+    return file_paths
 
-toc = ''
 
-for filepath in filepaths:
-    if 'checkpoints' in filepath or 'hw' in filepath:
-        continue
+def prepare_inner_title(raw_json_part: dict):
+    title = raw_json_part['source'][0].strip()
+    if len(title) > 1 and title[1] == '#':
 
-    filepath_prepared = filepath.replace('_', ' ').replace('.ipynb', '').replace('./', '').split('/')
-    print(filepath_prepared)
-    dir_lvl_1 = filepath_prepared[0].split(' ')[1].title()
-    toc += '1. ' + dir_lvl_1
-    tab_count = 1
-    for inner_name in filepath_prepared[2:]:
-        toc += '\n' + '\t' * tab_count + '1. ' + inner_name.title().split(' ')[1].title()
-        with open(filepath) as f:
-            file_content = json.loads(f.read())
+        return '\n' + title.replace('#', '\t')  # .replace(' ', '- ', 1)
+    else:
+        # return ' (**' + title.replace('#', '').strip() + '**)'
+        return f" ({title.replace('#', '').strip()})"
 
-            markdown_cells = ['\n' + x['source'][0].strip().replace(' ', '1. ', 1).replace('#', '\t')
-                              for x in file_content['cells']
-                              if x['cell_type'] == 'markdown' and len(x['source']) > 0 and x['source'][0][0] == '#']
 
-            toc += ''.join(markdown_cells)
+def is_md_title(raw_json_part: dict):
+    return raw_json_part['cell_type'] == 'markdown' \
+           and len(raw_json_part['source']) > 0 \
+           and raw_json_part['source'][0][0] == '#'
 
-    with open('./TOC.md', 'w') as toc_md:
-        toc_md.write(toc)
+
+def parse_inner_level(string: str):
+    title = ('\n' if string[0:2] != '00' else '') + '\t' + string.title()
+    with open(filepath) as f:
+        file_content = json.loads(f.read())
+
+        markdown_cells = [prepare_inner_title(x)
+                          for x in file_content['cells']
+                          if is_md_title(x)]
+
+    title += ''.join(markdown_cells)
+    return title
+
+
+if __name__ == '__main__':
+    filepaths = get_filepaths()
+    toc = ''
+    dir_lvl_1_titles = set()
+    for filepath in filepaths:
+        if 'checkpoints' in filepath or 'hw' in filepath:
+            continue
+
+        filepath_prepared = filepath.replace('_', ' ').replace('.ipynb', '').replace('./', '').split('/')
+        dir_lvl_1 = filepath_prepared[0].split(' ', 1)[1].replace('_', ' ').title()
+        if dir_lvl_1 not in dir_lvl_1_titles:
+            dir_lvl_1_len = len(dir_lvl_1_titles)
+            toc += ('\n' if dir_lvl_1_len else '') + f"{dir_lvl_1_len:02d} {dir_lvl_1}\n"
+            dir_lvl_1_titles.add(dir_lvl_1)
+        for inner_name in filepath_prepared[2:]:
+            toc += parse_inner_level(string=inner_name)
+
+        with open('./TOC.txt', 'w') as toc_md:
+            toc_md.write(toc)
