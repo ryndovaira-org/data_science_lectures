2a4296888ccecd1dedc4a0f017fb8e0880e7161a
ryndovaira <ryndovaira@gmail.com>
2023-03-09 15:57:56 +0300

    refactoring

--- create_toc.py
+++ create_toc.py
@@ -11,9 +11,9 @@ def get_filepaths():
 
 def prepare_inner_title(raw_json_part: dict):
     title = raw_json_part['source'][0].strip()
-    if len(title) > 3 and title[0:3] == '## ':
-        return '\n\t\t' + title.replace('#', '').replace(' ', '1. ', 1)
-    elif len(title) > 2 and title[0:2] == '# ':
+    # if len(title) > 3 and title[0:3] == '## ':
+    #     return '\n\t\t' + title.replace('#', '').replace(' ', '1. ', 1)
+    if len(title) > 2 and title[0:2] == '# ':
         return f" (**{title.replace('#', '').strip()}**)"
 
 
@@ -24,16 +24,20 @@ def is_md_title(raw_json_part: dict):
 
 
 def parse_inner_level(string: str):
-    title = f"\n\t1. {string.split(' ', 1)[1].capitalize()}"
-    with open(filepath) as f:
-        file_content = json.loads(f.read())
+    file_titles = string.split(' ', 1)
+    if len(file_titles) > 1:
+        title = f"\n\t1. {file_titles[1].capitalize()}"
+        with open(filepath) as f:
+            file_content = json.loads(f.read())
 
-        markdown_cells = [prepare_inner_title(x)
-                          for x in file_content['cells']
-                          if is_md_title(x)]
+            markdown_cells = [prepare_inner_title(x)
+                              for x in file_content['cells']
+                              if is_md_title(x)]
 
-    title += ''.join([cell for cell in markdown_cells if cell is not None])
-    return title
+        title += ''.join([cell for cell in markdown_cells if cell is not None])
+        return title
+    else:
+        return ''
 
 
 if __name__ == '__main__':
@@ -41,7 +45,7 @@ if __name__ == '__main__':
     toc = ''
     dir_lvl_1_titles = set()
     for filepath in filepaths:
-        if 'checkpoints' in filepath or 'hw' in filepath:
+        if 'checkpoints' in filepath:
             continue
 
         filepath_prepared = filepath.replace('_', ' ').replace('.ipynb', '').replace('./', '').split('/')
@@ -50,7 +54,7 @@ if __name__ == '__main__':
             dir_lvl_1_len = len(dir_lvl_1_titles)
             toc += ('\n' if dir_lvl_1_len else '') + f"1. {dir_lvl_1}"
             dir_lvl_1_titles.add(dir_lvl_1)
-        for inner_name in filepath_prepared[2:]:
+        for inner_name in filepath_prepared[1:]:
             toc += parse_inner_level(string=inner_name)
 
         with open('./TOC.md', 'w') as toc_md:
