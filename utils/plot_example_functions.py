e651d814d6d8817e3b47f0428ea0d7d9e85a7d03
ryndovaira <ryndovaira@gmail.com>
2023-03-29 20:40:54 +0400

    plot builder improvements

--- utils/plot_example_functions.py
+++ utils/plot_example_functions.py
@@ -2,12 +2,14 @@ from abc import abstractmethod
 from enum import Enum, auto
 from typing import Callable
 
+import numpy as np
+
 from .imports import *
 
 
 class Figure:
     AROUND_DECIMALS = 2
-    LIM_OFFSET = 1
+    LIM_OFFSET = 2
 
     class Type(Enum):
         ORIGINAL = auto()
@@ -30,27 +32,58 @@ class Figure:
         POINT_Y0 = auto()
 
     PARAMS = {
-        Type.ORIGINAL: dict(label='original', marker='.', c='#00E6B3', linewidth=6, alpha=0.5, zorder=1),
+        Type.ORIGINAL: dict(label='original',
+                            marker='.',
+                            c='#00E6B3',
+                            linewidth=6,
+                            alpha=0.5,
+                            zorder=1),
 
-        Type.DERIVATIVE_1: dict(label='', marker='.', c='#43A2E6', linewidth=6, zorder=1),
-        Type.DERIVATIVE_2: dict(label='', marker='.', c='#586F80', linewidth=6, zorder=1),
+        Type.DERIVATIVE_1: dict(label='derivative 1',
+                                marker='.',
+                                c='#43A2E6',
+                                linewidth=6,
+                                alpha=0.5,
+                                zorder=1),
+        Type.DERIVATIVE_2: dict(label='derivative 2',
+                                marker='.',
+                                c='#586F80',
+                                linewidth=6,
+                                alpha=0.5,
+                                zorder=1),
 
-        Type.INTERVAL_UP: dict(label='interval up', marker=',', c='#E61D00', linewidth=2, zorder=3),
-        Type.INTERVAL_DOWN: dict(label='interval down', marker=',', c='#3A30E6', linewidth=2, zorder=3),
+        Type.INTERVAL_UP: dict(label='interval up',
+                               marker=',',
+                               c='#E61D00',
+                               linewidth=2,
+                               alpha=0.5,
+                               zorder=3),
+        Type.INTERVAL_DOWN: dict(label='interval down',
+                                 marker=',',
+                                 c='#3A30E6',
+                                 linewidth=2,
+                                 alpha=0.5,
+                                 zorder=3),
 
-        Type.INTERVAL_ABOVE_0: dict(label='interval above 0', marker=',', c='#E65D6F', linewidth=1, zorder=4),
-        Type.INTERVAL_BELOW_0: dict(label='interval below 0', marker=',', c='#187099', linewidth=1, zorder=4),
+        Type.INTERVAL_ABOVE_0: dict(label='interval above 0',
+                                    marker=',',
+                                    c='#E65D6F',
+                                    linewidth=1,
+                                    alpha=0.5,
+                                    zorder=4),
+        Type.INTERVAL_BELOW_0: dict(label='interval below 0', marker=',', c='#187099', linewidth=1, alpha=0.5,
+                                    zorder=4),
 
         Type.COORDINATE_INTERVAL_X: dict(label='D(y)', marker='.', c='#E5E180', linewidth=2, alpha=0.25, zorder=0),
         Type.COORDINATE_INTERVAL_Y: dict(label='E(y)', marker='.', c='#E5E180', linewidth=2, alpha=0.25, zorder=0),
 
-        Type.POINT_MAX: dict(label='Max', marker='*', c='#C0E62C', alpha=0.35, s=50, zorder=2),
-        Type.POINT_MIN: dict(label='Min', marker='*', c='#37E651', alpha=0.35, s=50, zorder=2),
+        Type.POINT_MAX: dict(label='max', marker='*', c='#C0E62C', alpha=0.35, s=50, zorder=2),
+        Type.POINT_MIN: dict(label='min', marker='*', c='#37E651', alpha=0.35, s=50, zorder=2),
 
         Type.POINT_Y0: dict(label='y=0', marker='o', c='#069900', alpha=0.35, s=150, zorder=2)
     }
 
-    def __init__(self, ax: axes.Axes, x: float | np.ndarray, y: float | np.ndarray, obj_type: Type):
+    def __init__(self, ax: axes.Axes, x: np.ndarray, y: np.ndarray, obj_type: Type):
         self.ax = ax
         self.x = x
         self.y = y
@@ -60,7 +93,7 @@ class Figure:
         ...
 
 
-class Point(Figure):
+class Points(Figure):
     def draw(self):
         self.ax.scatter(x=self.x,
                         y=self.y,
@@ -119,7 +152,6 @@ class Curve(Interval):
             self.ax.set_ylim(self.min_y,
                              ax_y_max)
 
-
     @property
     def rounded_x(self):
         return np.around(self.x, self.AROUND_DECIMALS)
@@ -129,10 +161,11 @@ class Curve(Interval):
         return np.around(self.y, self.AROUND_DECIMALS)
 
     def curve_analysis(self):
-        figures = [self.get_d_y(), self.get_e_y()]
-        figures.extend(self.get_x_where_y_below_above_0())
-        figures.extend(self.get_curve_up_down())
-        figures.extend(self.get_xs_where_y_eq_0())
+        figures = [self.get_d_y(),
+                   self.get_e_y(),
+                   self.get_xs_where_y_eq_0(),
+                   *self.get_x_where_y_below_above_0(),
+                   *self.get_curve_up_down()]
         return figures
 
     def get_max_min_point(self):
@@ -150,27 +183,22 @@ class Curve(Interval):
                         y=self.y,
                         obj_type=Figure.Type.COORDINATE_INTERVAL_Y)
 
-    def eliminate_point_duplicates(self, points):
-        sorted_points = sorted(points, key=lambda point: point.x)
-        points_x = [point.x for point in sorted_points]
-        diff = np.ediff1d(points_x)
+    def get_xs_where_y_eq_0(self):
+        sorted_xs = sorted([xi for xi in self.rounded_x[np.where(self.rounded_y == 0)]])
+        diff = np.ediff1d(sorted_xs)
         threshold = 0.15
         if np.count_nonzero(diff < threshold):
             to_keep = np.where(diff >= threshold)
             if np.count_nonzero(to_keep) == 0:
-                median = np.median(points_x)
-                return [Point(ax=self.ax,
-                              x=median,
-                              y=np.around(self.formula(median), self.AROUND_DECIMALS),
-                              obj_type=points[0].obj_type)]
-        return sorted_points
-
-    def get_xs_where_y_eq_0(self):
-        return self.eliminate_point_duplicates([Point(ax=self.ax,
-                                                      x=xi,
-                                                      y=0,
-                                                      obj_type=Figure.Type.POINT_Y0)
-                                                for xi in self.rounded_x[np.where(self.rounded_y == 0)]])
+                median = np.median(sorted_xs)
+                return Points(ax=self.ax,
+                              x=np.asarray(median),
+                              y=np.asarray(0),
+                              obj_type=Figure.Type.POINT_Y0)
+        return Points(ax=self.ax,
+                      x=np.asarray(sorted_xs),
+                      y=np.full((1, len(sorted_xs)), 0),
+                      obj_type=Figure.Type.POINT_Y0)
 
     def get_x_where_y_below_above_0(self):
         tmp_xy = {Figure.Type.INTERVAL_ABOVE_0: [],
