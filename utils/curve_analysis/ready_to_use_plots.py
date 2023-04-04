12af6e43fc821c0cbe9b83fc13811fcf9e7eb045
ryndovaira <ryndovaira@gmail.com>
2023-04-05 00:23:06 +0400

    update differential calculus lectures

--- utils/curve_analysis/ready_to_use_plots.py
+++ utils/curve_analysis/ready_to_use_plots.py
@@ -11,14 +11,14 @@ def discontinuous_functions():
     curves = {
         r"infinite": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 label=r"$\frac{1}{x}$",
                 x_min=-4,
                 x_max=0,
                 formula=lambda x: 1 / x,
             ),
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 label=r"$\frac{1}{x}$",
                 x_min=0,
                 x_max=4,
@@ -27,14 +27,14 @@ def discontinuous_functions():
         ),
         r"jump": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 label=r"$x^2$",
                 x_min=-2,
                 x_max=0.9,
                 formula=lambda x: x * x,
             ),
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 label=r"$x^2 + 1$",
                 x_min=1.1,
                 x_max=2,
@@ -57,14 +57,14 @@ def discontinuous_functions():
         ),
         r"removable": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 label=r"$x^2 + 1$",
                 x_min=-2,
                 x_max=0.9,
                 formula=lambda x: x * x + 1,
             ),
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 label=r"$x^2 + 1$",
                 x_min=1.1,
                 x_max=2,
@@ -80,7 +80,7 @@ def discontinuous_functions():
         ),
         r"endpoint": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 label=r"$\sqrt{x}$",
                 x_min=0,
                 x_max=4,
@@ -102,14 +102,14 @@ def sign_function():
     curves = {
         "sign(x)": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 x_min=-4,
                 x_max=-0.1,
                 formula=np.sign,
                 label="$sign(x)$",
             ),
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 x_min=0.1,
                 x_max=4,
                 formula=np.sign,
@@ -131,21 +131,21 @@ def example_functions_to_analyse():
     curves_to_analyse = {
         "sin(x)": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 x_min=-1.5 * np.pi,
                 x_max=1.5 * np.pi,
                 formula=np.sin,
                 label=r"$sin(x)$",
             ),
             dict(
-                obj_type=Curve.Type.DERIVATIVE_1,
+                obj_type=Curve.Type.MAIN_1,
                 x_min=-1.5 * np.pi,
                 x_max=1.5 * np.pi,
                 formula=np.cos,
                 label=r"$sin'(x) = cos(x)$",
             ),
             dict(
-                obj_type=Curve.Type.DERIVATIVE_2,
+                obj_type=Curve.Type.MAIN_2,
                 x_min=-1.5 * np.pi,
                 x_max=1.5 * np.pi,
                 formula=lambda x: -np.sin(x),
@@ -154,21 +154,21 @@ def example_functions_to_analyse():
         ),
         r"$x ^ 2$": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 x_min=-4,
                 x_max=4,
                 formula=np.square,
                 label=r"$x ^ 2$",
             ),
             dict(
-                obj_type=Curve.Type.DERIVATIVE_1,
+                obj_type=Curve.Type.MAIN_1,
                 x_min=-4,
                 x_max=4,
                 formula=lambda x: 2 * x,
                 label=r"$x^{2}' = 2x$",
             ),
             dict(
-                obj_type=Curve.Type.DERIVATIVE_2,
+                obj_type=Curve.Type.MAIN_2,
                 x_min=-4,
                 x_max=4,
                 formula=lambda x: 2 + 0 * x,
@@ -177,21 +177,21 @@ def example_functions_to_analyse():
         ),
         f"$x^3$": (
             dict(
-                obj_type=Curve.Type.MAIN,
+                obj_type=Curve.Type.MAIN_0,
                 x_min=-4,
                 x_max=4,
                 formula=lambda x: x * x * x,
                 label=r"$x^{3}$",
             ),
             dict(
-                obj_type=Curve.Type.DERIVATIVE_1,
+                obj_type=Curve.Type.MAIN_1,
                 x_min=-4,
                 x_max=4,
                 formula=lambda x: 3 * x * x,
                 label=r"$x^{3}' = 3x^2$",
             ),
             dict(
-                obj_type=Curve.Type.DERIVATIVE_2,
+                obj_type=Curve.Type.MAIN_2,
                 x_min=-4,
                 x_max=4,
                 formula=lambda x: 6 * x,
@@ -201,6 +201,243 @@ def example_functions_to_analyse():
     }
 
     PlotBuilder(curves_to_analyse, True, 12, 9)
+
+
+def power_functions():
+    curves_x_3 = {
+        r"$x^3$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: x**3,
+                label="$x^3$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: x**3 - 50,
+                label="$x^3 - 50$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_2,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: x**3 + 50,
+                label="$x^3 + 50$",
+            ),
+        ),
+    }
+    curves_x_6 = {
+        "$x^6$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: x**6,
+                label="$x^6$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: 4 * (x**6),
+                label="$4 * x^6$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_2,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: 0.25 * (x**6),
+                label="$0.25 * x^6$",
+            ),
+        ),
+    }
+    curves_x_pow_frac = {
+        r"$x^\frac{1}{4}$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=0,
+                x_max=4,
+                formula=lambda x: x ** (1 / 4),
+                label=r"$x^\frac{1}{4}$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=0,
+                x_max=4,
+                formula=lambda x: 4 * x ** (1 / 4),
+                label=r"$4 * x^\frac{1}{4}$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_2,
+                x_min=0,
+                x_max=4,
+                formula=lambda x: 0.25 * x ** (1 / 4),
+                label=r"$0.25 * x^\frac{1}{4}$",
+            ),
+        ),
+        r"$x^\frac{3}{2}$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: x ** (3 / 2),
+                label=r"$x^\frac{3}{2}$",
+            ),
+        ),
+    }
+
+    curves_x_minus = {
+        r"$x^{-3}$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-0.1,
+                x_max=-0.01,
+                formula=lambda x: x ** (-3),
+                label=r"$x^{-3}$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=0.01,
+                x_max=0.1,
+                formula=lambda x: x ** (-3),
+                label=r"$x^{-3}$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=-0.1,
+                x_max=-0.01,
+                formula=lambda x: 2 * x ** (-3),
+                label=r"$4 * x^{-3}$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=0.01,
+                x_max=0.1,
+                formula=lambda x: 2 * x ** (-3),
+                label=r"$4 * x^{-3}$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_2,
+                x_min=-0.1,
+                x_max=-0.01,
+                formula=lambda x: 0.25 * x ** (-3),
+                label=r"$0.25 * x^{-3}$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_2,
+                x_min=0.01,
+                x_max=0.1,
+                formula=lambda x: 0.25 * x ** (-3),
+                label=r"$0.25 * x^{-3}$",
+            ),
+        )
+    }
+    PlotBuilder(curves_x_3, False)
+    PlotBuilder(curves_x_6, False)
+    PlotBuilder(curves_x_pow_frac, False)
+    PlotBuilder(curves_x_minus, False)
+
+
+def exponential_functions():
+    curves = {
+        "$a^x$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: 2**x,
+                label="$2^x$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: 0.5**x,
+                label="$0.5^x$",
+            ),
+        ),
+    }
+
+    PlotBuilder(curves, False)
+
+
+def logarithm_functions():
+    curves = {
+        r"$\log_{2}x$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: np.log2(x),
+                label=r"$\log_{2}x$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: np.log2(-x),
+                label=r"$\log_{2}(-x)$",
+            ),
+        ),
+    }
+
+    PlotBuilder(curves, False)
+
+
+def trigonometric_functions():
+    curves = {
+        r"$\sin & \cos$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=np.sin,
+                label=r"$\sin(x)$",
+            ),
+            dict(
+                obj_type=Curve.Type.MAIN_1,
+                x_min=-4,
+                x_max=4,
+                formula=np.cos,
+                label=r"$\cos(x)$",
+            ),
+        ),
+    }
+
+    PlotBuilder(curves, False)
+
+
+def identity_function():
+    curves = {
+        r"$identity functon$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=lambda x: x,
+                label=r"$f(x) = x$",
+            ),
+        ),
+    }
+    PlotBuilder(curves, False)
+
+
+def absolute_value():
+    curves = {
+        r"$abs$": (
+            dict(
+                obj_type=Curve.Type.MAIN_0,
+                x_min=-4,
+                x_max=4,
+                formula=abs,
+                label=r"$f(x) = |x|$",
+            ),
+        ),
+    }
+    PlotBuilder(curves, False)
 
 
 if __name__ == "__main__":
