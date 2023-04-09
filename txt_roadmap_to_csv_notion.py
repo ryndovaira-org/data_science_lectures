2499299b58bf5ee19b79b963bb64026491875bde
ryndovaira <ryndovaira@gmail.com>
2023-04-09 18:06:57 +0300

    update roadmap creation

--- txt_roadmap_to_csv_notion.py
+++ txt_roadmap_to_csv_notion.py
@@ -1,13 +1,17 @@
 new_file = ""
-new_first_line = "1, 2, 3, 4\n"
-with open("C:/Users/Mi/Desktop/roadmap.txt", "r") as file:
+new_first_line = "Title, Group, GitHub\n"
+with open("./roadmap.txt", "r") as file:
     for line in file.readlines():
         path = line.strip()
-        tmp = path.replace("/", ", ", 2).split(", ")
+        group, title = path.rsplit("/", 1)
         github_link = (
             f"https://github.com/ryndovaira-org/data_science_notes/tree/main/{path}"
         )
-        new_file += f"{tmp[0]}, {tmp[2]}, {tmp[1]}, {github_link}\n"
+        group_num, group_name = group.split('_', 1)
+        title_num, title_name = title.split('_', 1)
+        if len(group_num) == 0 or len(title_num) == 0:
+            continue
+        new_file += f"{title}, {group}, {github_link}\n"
 
-with open("C:/Users/Mi/Desktop/roadmap_new.csv", "w") as file:
+with open("./roadmap_notion.csv", "w") as file:
     file.write(new_first_line + new_file)
