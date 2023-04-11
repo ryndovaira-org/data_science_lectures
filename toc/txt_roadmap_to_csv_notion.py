ad19175e2b903b02b0d88842b1c67bb013f0bd78
ryndovaira <ryndovaira@gmail.com>
2023-04-11 16:26:01 +0300

    refactoring

---
+++ toc/txt_roadmap_to_csv_notion.py
@@ -0,0 +1,17 @@
+new_file = ""
+new_first_line = "Title, Group, GitHub\n"
+with open("./roadmap.txt", "r") as file:
+    for line in file.readlines():
+        path = line.strip()
+        group, title = path.rsplit("/", 1)
+        github_link = (
+            f"https://github.com/ryndovaira-org/data_science_notes/tree/main/{path}"
+        )
+        group_num, group_name = group.split('_', 1)
+        title_num, title_name = title.split('_', 1)
+        if len(group_num) == 0 or len(title_num) == 0:
+            continue
+        new_file += f"{title}, {group}, {github_link}\n"
+
+with open("./roadmap_notion.csv", "w") as file:
+    file.write(new_first_line + new_file)
