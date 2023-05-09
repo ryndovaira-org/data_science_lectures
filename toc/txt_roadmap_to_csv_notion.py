b92dc3338a20175843569a7f1d63ae536d9dc4a7
ryndovaira <ryndovaira@gmail.com>
2023-05-09 11:05:59 +0300

    black format

--- toc/txt_roadmap_to_csv_notion.py
+++ toc/txt_roadmap_to_csv_notion.py
@@ -7,8 +7,8 @@ with open("./roadmap.txt", "r") as file:
         github_link = (
             f"https://github.com/ryndovaira-org/data_science_notes/tree/main/{path}"
         )
-        group_num, group_name = group.split('_', 1)
-        title_num, title_name = title.split('_', 1)
+        group_num, group_name = group.split("_", 1)
+        title_num, title_name = title.split("_", 1)
         if len(group_num) == 0 or len(title_num) == 0:
             continue
         new_file += f"{title}, {group}, {github_link}\n"
